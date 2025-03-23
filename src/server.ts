791257b576f34a19589220d9ab31d831ba70b2c5
ssantana <ssantana2423@gmail.com>
2025-03-23 10:41:37 -0500

    intial ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+import mongoose from 'mongoose'
+import express from 'express'
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  app.listen(3000)
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
